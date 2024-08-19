22b1dc4baedee1a753cc6b562e08df5c5ad3d690
backiya1096 <backiyalakshmi1096@gmail.com>
2024-08-19 13:19:59 +0530

    design ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
     theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "darkpurple": "#481380"
+      },
+      fontFamily: {
+        "roboto": "'Roboto', sans-serif"
+      }
+    },
   },
   plugins: [],
 }
